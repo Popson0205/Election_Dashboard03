19fbf86994d5be79a74401101c58fef126c51c52
Popoola Idris Bamigboye <idrispopoola02@gmail.com>
2026-03-05 14:29:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -496,7 +496,7 @@ DASHBOARD_HTML = """
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Accord Situation Room - LIVE</title>
+    <title>Imole Youth Accord Mobilization Situation Room - LIVE</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
